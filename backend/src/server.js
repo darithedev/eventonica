100029868af9dea17cf0bc9b5b3f162c8dfeba61
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-23 23:56:40 -0500

    Initalize backend with cors, express, pg, dotenv, nodemon, and vitest depencancies

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import pool from './db/pools.js'
+import dotenv from 'dotenv'
+
+const app = express()
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async(req, res) => {
+    try {
+        res.status(200).json({ message: 'Express server is healthy.' });
+    } catch (error) {
+        res.status(200).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, '127.0.0.1', () => {
+    ({ message: `Express server is running on port ${PORT}` })
+})
