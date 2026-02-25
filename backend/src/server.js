f4fc94004e9bf8887eca01dd49fb4b0bdfbdeb2c
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-24 22:29:50 -0500

    Add delete route for removing event by id

--- backend/src/server.js
+++ backend/src/server.js
@@ -96,6 +96,24 @@ app.patch('/events/:id', async (req, res) => {
     }
 });
 
+// Endpoint to delete an event
+app.delete('/events/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            'DELETE FROM event WHERE id=$1 RETURNING *', 
+            [id]
+        );
+
+        if (result.rows.length === 0) return res.status(404).json({ error: 'This event does not exist!'});
+
+        res.status(200).json({ message: `The event "${result.rows[0].event_name}" was deleted!` })
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 app.listen(PORT, '127.0.0.1', () => {
     console.log(`Express server is running on port ${PORT}`);
 });
