67b81c240109822add7e54c7cfd889637898dc6e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-27 17:07:53 -0500

    Refactor ListStudents file name to ListEvents

---
+++ frontend/src/components/ListEvents.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import * as ioicons from "react-icons/io5";
+import EventForm from "./Form.jsx";
+import Event from "./Event.jsx";
+
+const ListEvents = () => {
+    const [events, setEvents] = useState([]);
+    const [editingEvent, setEditingEvent] = useState(null);
+    const loadEvents = () => {
+      fetch("http://localhost:8080/api/events")
+        .then((response) => response.json())
+        .then((events) => {
+          setEvents(events);
+        });
+    };
+    const [isNewEvent, setIsNewEvent] = useState(false);
+
+    useEffect(() => {
+        loadEvents();
+    }, []);
+
+    const onSaveEvent = (newEvent) => {
+      setEvents((events) => [...events, newEvent]);
+    };
+
+    const updateEvent = (updatedEvent) => {
+      setEvents((events) =>
+        events.map((event) =>
+            event.id === updatedEvent.id ? updatedEvent : event
+        )
+      );
+    };
+
+    const onDelete = (event) => {
+        return fetch(`http://localhost:8080/api/event/${event.id}`, {
+            method: "DELETE"
+        }).then((response) => {
+            if (response.ok) {
+                loadEvents();
+            }
+        });
+    };
+
+    const onUpdate = (toUpdateEvent) => {
+        setEditingEvent(toUpdateEvent);
+
+    };
+
+    return (
+        <div className="mybody">
+            {(isNewEvent || editingEvent)
+                ? (<EventForm
+                    key={editingEvent ? editingEvent.id : null}
+                    onSaveEvent={onSaveEvent}
+                    editingEvent={editingEvent}
+                    onUpdateEvent={updateEvent}
+                    setIsNewEvent={setIsNewEvent}
+                    setEditingEvent={setEditingEvent}
+                />
+                ) : (<div className="list-events">
+                        <h2>Eventonica Events</h2>
+                        <button onClick={() => setIsNewEvent(true)}>Add Event</button>
+                        <ul>
+                            {events.map((event) => {
+                                return (
+                                <li key={event.id}>
+                                    {" "}
+                                    <Event
+                                        event={event}
+                                        toDelete={onDelete}
+                                        toUpdate={updateEvent}
+                                        onEdit={() => setEditingEvent(event)}
+                                    />
+                                </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                )
+            }
+        </div>
+    );
+};
+
+export default ListEvents;
