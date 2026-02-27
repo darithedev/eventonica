1296923bd5a801bd949a296ed97978639003e798
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-27 07:26:08 -0500

    Add Event.jsx

---
+++ frontend/src/components/Event.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import * as ioicons from "react-icons/io5";
+
+const Student = ({ event, toUpdate, toDelete }) => {
+
+  return (
+    <Card>
+      
+    </Card>
+  );
+};
+
+export default Student;
