e9a8d436f13d2fbd83917ceaefce46bed07f869f
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-27 07:30:09 -0500

    Add onUpdate function

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -4,6 +4,9 @@ import Button from "react-bootstrap/Button";
 import * as ioicons from "react-icons/io5";
 
 const Event = ({ event, toUpdate, toDelete }) => {
+    const onUpdate = (toUpdateEvent) => {
+        toUpdate(toUpdateEvent);
+    }
 
   return (
     <Card>
