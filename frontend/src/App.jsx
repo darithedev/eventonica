d355a4bc2e5a566df7382426ac5bd7f335a4249e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-27 17:04:10 -0500

    Import ListEvents component and render to page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ListEvents from './components/ListEvents.jsx'
 
 function App() {
 
   return (
     <>
+      <ListEvents />
     </>
   )
 }
