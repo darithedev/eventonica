ad777b683ea176eb2ce3dcaefffcb6d90bbfea3f
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-02-27 07:21:49 -0500

    Add ListStudents component:

---
+++ frontend/src/components/ListStudents.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import * as ioicons from "react-icons/io5";
+import EventForm from "./Form.jsx";
+import Event from "./Event.jsx";
+
+const ListEvents = () => {
+
+  return (
+    <div className="mybody">
+
+    </div>
+  );
+};
+
+export default ListEvents;
